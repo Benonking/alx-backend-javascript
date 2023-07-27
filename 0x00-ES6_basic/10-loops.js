ea6130ce86cddb2a57b0e2930b16bff8a754d5c1
Benonking <benonkighoma@gmail.com>
2023-07-27 20:11:47 +0300

    impement for loop on array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arry = []
+  for (const idx of array) {
+    
+    arry.push(appendString + idx);
+  }
+
+  return arry;
+}
