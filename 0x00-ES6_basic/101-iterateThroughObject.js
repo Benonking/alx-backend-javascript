0bb0727f27b1d9984c62a62102564502c0535aab
Benonking <benonkighoma@gmail.com>
2023-07-28 20:28:00 +0300

    use join on strings

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const empNames = [];
+  for (const name of reportWithIterator) {
+    empNames.push(name);
+  }
+  return empNames.join(' | ');
+}
