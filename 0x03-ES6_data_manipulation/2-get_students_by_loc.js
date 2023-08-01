7833d4b02226cb4de98406cbf4d7c901d9989758
Benonking <benonkighoma@gmail.com>
2023-08-01 18:54:00 +0300

    fix esLint

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,5 @@
 export default function getStudentsByLocation(arr, city) {
   if (Array.isArray(arr) !== true) return [];
-  const filterByLocation = arr.filter((student) => {
-    if (student.location === city) {
-      return student;
-    }
-  });
-  return filterByLocation;
+  return arr.filter((student) => { if (student.location === city) return student;
+  })
 }
