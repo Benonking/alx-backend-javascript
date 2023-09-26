5114609f4a7fde2db52f3053641da412c5b6d9c6
Benonking <benonkighoma@gmail.com>
2023-09-26 17:39:09 +0300

    Combine two promisses into one using Promisses.all function and display the contents for thenew promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,9 @@
 import { uploadPhoto, createUser} from './utils';
 export default function handleProfileSignup() {
+  /*Promise .all takes iterable of promisses as 
+  input and returns a single promise*/
   return Promise.all([uploadPhoto(), createUser()])
   .then((values) => {
-    const { body } = values[0];
-    const { firstName }= values[1];
-    const { lastName } = values[2];
-    console.log(`${body} ${firstName} ${lastName}`);
-  })
-  .catch(() => console.log('Signup system offline'));
-
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  });
 }
--- 0x01-ES6_promise/README.md
+++ 0x01-ES6_promise/README.md
@@ -1,1 +1,14 @@
 # ES6 Promises
+In this project we axplore all the possible use cases in whicj to use Javascript Promises
+## Learning Objectives
+- Promises (how, why, and what)
+- How to use the then, resolve, catch methods
+- How to use every method of the Promise object
+- Throw / Try
+- The await operator
+- How to use an async function
+## Linter
+- Jest (npm run test)
+## Tester
+-- ESlint
+Node Version 12.11.X
