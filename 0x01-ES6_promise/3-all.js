1bc2691d2f976d66fdf531d6a1dfea549943f9cc
Benonking <benonkighoma@gmail.com>
2023-09-27 08:34:48 +0300

    Combine two promisses into one using Promisses.all function and display the contents for thenew promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    /* Promise .all takes iterable of promisses as 
-    input and returns a single promise */
     return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
         console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
