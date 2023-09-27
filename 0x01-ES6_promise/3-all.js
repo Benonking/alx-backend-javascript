7269423129b041dee99d55a07449204df6057827
Benonking <benonkighoma@gmail.com>
2023-09-27 08:08:08 +0300

    Combine two promisses into one using Promisses.all function and display the contents for thenew promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,13 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
     /* Promise .all takes iterable of promisses as 
     input and returns a single promise */
-    try {
-        return Promise.all([uploadPhoto(), createUser()])
-        .then((values) => {
-            console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-        });
-    }
-    catch(error){
-        console.log('Singup system offline');
-    }
+    return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+        console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Singup system offline'))
 }
