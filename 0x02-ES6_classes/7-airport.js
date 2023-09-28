77c30d1b06a0c7cce6a139f563b8814f67fe5739
Benonking <benonkighoma@gmail.com>
2023-09-28 14:58:11 +0300

    return class atrributes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,22 +1,26 @@
 export default class Airport {
   constructor(name, code) {
-
+    this.name = name;
+    this.code = code;
   }
+
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name === 'string') {
-      this._name = name
+      this._name = name;
     }
   }
+
   get code() {
     return this._code;
   }
+
   set code(code) {
     if (typeof code === 'string') {
       this._code = code;
     }
   }
-
 }
