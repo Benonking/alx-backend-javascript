6b80aca4e20430daac9f72c4bbf453a4a02ea6fc
Benonking <benonkighoma@gmail.com>
2023-09-29 17:25:02 +0300

    change to string tag

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,10 @@
 export default class Airport {
   constructor(name, code) {
-    this.name = name;
-    this.code = code;
+    this._name = name;
+    this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    }
-  }
-
-  get code() {
+  get [Symbol.toStringTag]() {
     return this._code;
-  }
-
-  set code(code) {
-    if (typeof code === 'string') {
-      this._code = code;
-    }
   }
 }
