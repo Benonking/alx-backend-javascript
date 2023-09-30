f0b65882f140c8e5e67a562517e66272101552ce
Benonking <benonkighoma@gmail.com>
2023-09-30 09:29:19 +0300

    child must overide function of super class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,21 @@
 export default class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error('Class extending Building must ovveride evacuationWarningMessage');
-    }
-    this._sqft = sqft;
+    this.sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      if (
+        this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function'
+      ) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+      this._sqft = sqft;
+    }
   }
 }
