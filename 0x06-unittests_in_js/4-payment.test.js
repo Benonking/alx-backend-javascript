570d27e0dfd349def894ce6f5818cd44a8909b0c
Benonking <benonkighoma@gmail.com>
2023-11-23 17:27:21 +0300

    use of stub to assume resultls of an expensive function

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+expect = chai.expect;
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with stub', () => {
+    // Create a spy on Utils.calculateNumber
+    const stub = sinon.stub(Utils, 'calculateNumber');
+		const consoleSpy = sinon.spy(console, 'log');
+		stub.returns(10)
+    // Call the function that uses Utils.calculateNumber
+    const res = sendPaymentRequestToApi(100, 20);
+
+    // Verify that Utils.calculateNumber was called with the correct arguments
+    expect(consoleSpy.calledWithExactly('The total is: 10')).to.equal(true)
+		expect(consoleSpy.calledOnce).to.be.true;
+		expect(stub.withArgs('SUM', 100, 20).calledOnce).to.be.true;
+		expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+		expect(console.log(`The total is: ${Utils.calculateNumber('SUM', 100, 20)}`)).to.equal(res);
+    // Restore the original function after testing
+    stub.restore();
+		consoleSpy.restore();
+  });
+});
