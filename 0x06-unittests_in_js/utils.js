47b8f82c740cd6e3e4a11910f6f4f06e65670f31
Benonking <benonkighoma@gmail.com>
2023-11-23 16:42:20 +0300

    utiity function

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+class Utils {
+  static calculateNumber = (type, a, b) => {  
+    const types = ['SUM','DIVIDE','SUBTRACT']
+		if (type == types[0]) {
+			return Math.round(a) + Math.round(b)
+		}
+		if (type == types[1]){
+			if (Math.round(b) == 0){
+				return 'Error'
+			}
+			return Math.round(a) / Math.round(b)
+		}
+		if (type == types[2]){
+			return Math.round(a) - Math.round(b)
+		}
+	}
+}
+
+module.exports = Utils;
