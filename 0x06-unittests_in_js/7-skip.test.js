fc14fd23c064f76c77c741105c4e670788dc8dbe
Benonking <benonkighoma@gmail.com>
2023-11-24 08:44:35 +0300

    skip tests and file ticket

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,7 +9,7 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it.skip('1 is equal to 3 -Failing Test (SKIP AND FILE A TICKET)', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
